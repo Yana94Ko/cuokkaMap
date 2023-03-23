626d4eae6526c6ef5b0145dccc686f1f57fbd836
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2023-03-23 18:46:10 +0900

    프로젝트 생성 - DB connection 등 민감정보 암호화 진행중. 1) git secret과 action활용한 암호화 진행 시도중.

---
+++ src/main/java/com/bside/cuokkamap/CuokkaMapApplication.java
@@ -0,0 +1,13 @@
+package com.bside.cuokkamap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuokkaMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuokkaMapApplication.class, args);
+	}
+
+}
