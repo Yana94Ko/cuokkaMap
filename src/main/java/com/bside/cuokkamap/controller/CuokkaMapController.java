e0b566b9ed6cced8e8fa534c21c425bb2d680900
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2023-03-25 16:38:47 +0900

    [BE] FEAT : 주석생성 테스트 종료

--- src/main/java/com/bside/cuokkamap/controller/CuokkaMapController.java
+++ src/main/java/com/bside/cuokkamap/controller/CuokkaMapController.java
@@ -6,25 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController //bean으로 생성
 public class CuokkaMapController {
-    // TODO: TODO git 액션 추가 (백엔드 JAVA 언어 환경).
-    //
-    // 이번 시도는 백엔드 파트에서 TODO의 이슈생성 테스트입니다.
-    //
-    // - `항목화`
-    // - `항목화`
-
-    // TODO(참조): 어사이니 추가 테스트
-    //  Everyone uses hello world and it's boring.
-    // assignees: hwanyb, sungsilver, hbjydev,Yana94Ko
-
     @GetMapping("/api/hello")
     public String getCuokka() {
         String msg = "/img/cuokkaMap_img.png";
         return msg;
     }
-    // TODO: TODO git 액션 추가2 (백엔드 JAVA 언어 환경).
-    //
-    // 이번 시도는 백엔드 파트에서 TODO의 이슈생성 테스트입니다.
 
 }
 
