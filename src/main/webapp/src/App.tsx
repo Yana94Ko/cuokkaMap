c7f38ecb70e35262bb4520b05904756aac289965
BABY\SEONGEUN <yooseongeun@naver.com>
2023-03-25 16:12:47 +0900

    [FE] Feat : 1차스크럼 역할분담완료 및 컴포넌트 생성

--- src/main/webapp/src/App.tsx
+++ src/main/webapp/src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import Login from "./components/login/Login";
+import KakaoLogin from "./components/home/KakaoLogin";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<HomePage/>}/>
-                <Route path="/login" element={<Login/>}/>
+                <Route path="/login" element={<KakaoLogin/>}/>
             </Routes>
         </BrowserRouter>
     );
