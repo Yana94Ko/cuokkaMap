936d1123a11fd012f93252466cdb25c70de4556b
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2023-03-25 15:44:36 +0900

    [BE] FEAT : todo 주석의 action 자동화 관련 테스트

--- src/main/java/com/bside/cuokkamap/controller/CuokkaMapController.java
+++ src/main/java/com/bside/cuokkamap/controller/CuokkaMapController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController //bean으로 생성
 public class CuokkaMapController {
+    // TODO: TODO git 액션 추가 (백엔드 JAVA 언어 환경).
+    //
+    // 이번 시도는 백엔드 파트에서 TODO의 이슈생성 테스트입니다.
+    //
+    // - `항목화`
+    // - `항목화`
 
     @GetMapping("/api/hello")
     public String getCuokka() {
