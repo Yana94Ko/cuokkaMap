e3ff768139d651a5264e568a8e1a8f4a0ff9a1b1
BABY\SEONGEUN <yooseongeun@naver.com>
2023-03-25 19:43:15 +0900

    [FE] Feat : searchInput useRef onChangeHandler 작업까지 완료

--- src/main/webapp/src/components/home/Header.tsx
+++ src/main/webapp/src/components/home/Header.tsx
@@ -1,29 +1,48 @@
-import React from 'react';
+import React, {RefObject, useRef} from 'react';
 import {Link} from "react-router-dom";
+import styled from "styled-components";
 const Header = () => {
-    // const searcBox = styled.div`
-    //   position:absolute;
-    //   top:10;
-    //   left:10;
-    //   z-index:100;
-    // `;
+    const searchPlaceInputRef = useRef<HTMLInputElement>(null);
+    // //검색하면 axios로 백에 요청 보낼거시기
+    const searchPlaceChangeHandler = (event: React.ChangeEvent<HTMLInputElement>):void => {
+        console.log(event.target.value)
+    }
+
+    const Base = styled.div`
+      width:calc(100vw - 100px);
+    {/*스크롤 없애야댐*/}
+      position:fixed;
+      z-index:100;
+    `;
+
+    const HeaderBox = styled.div`
+      width:100%;
+      margin:10px 50px;
+      display:flex;
+      justify-content: space-between;
+      form{
+        background:white;
+        border-radius:10px;
+        input{
+          width:20vw;
+          height:3em;
+          margin:10px;
+          border:none;
+          outline:none;
+        }
+      }
+    `;
 
     return (
-        <header style={{width:"80vw", position:"absolute", top:"10", left:"10", zIndex:"100"}}>
-            <div style={{display:"flex", justifyContent:"space-between"}}>
+        <Base>
+            <HeaderBox>
             <form>
-                <input type="search" name="search" spellCheck="false"/>
-                <input type="submit" value="검색"/>
+                <input type="text"  ref={searchPlaceInputRef} onChange={searchPlaceChangeHandler}/>&emsp;
+                <button>검색아이콘들어갈자리</button>
             </form>
             <Link to="/login">로그인</Link>
-            </div>
-            <div>
-                <button>락토프리</button>
-                <button>디카페인</button>
-                <button>두유</button>
-                <button>맛있는거</button>
-            </div>
-        </header>
+            </HeaderBox>
+        </Base>
     )
 }
 
