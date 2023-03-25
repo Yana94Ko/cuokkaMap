d52b89794802895b6d72149d8a3163a8773578d8
hwanyb <mnn9502@naver.com>
2023-03-25 17:22:17 +0900

    pull frontend -> hwanyb

--- src/main/webapp/src/App.tsx
+++ src/main/webapp/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import KakaoMap from "./components/home/KakaoMap";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import KakaoLogin from "./components/home/KakaoLogin";
 
 function App() {
-    return (
-        <div className="App">
-            <KakaoMap/>
-        </div>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/login" element={<KakaoLogin/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
 export default App;
