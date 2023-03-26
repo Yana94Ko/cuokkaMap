02417bcced5c2c5afa6098b020b9d6bc9b104026
hwanyb <mnn9502@naver.com>
2023-03-26 17:49:42 +0900

    [FE] Feat : 배너 컴포넌트 구성

--- src/main/webapp/src/components/home/Banner.tsx
+++ src/main/webapp/src/components/home/Banner.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import styled from "styled-components";
 
+import Icon from "../common/Icon";
+
+const Base = styled.div`
+  background-color: #fff;
+  position: fixed;
+  bottom: 50px;
+  right: 50px;
+  border-radius: 10px;
+  width: 300px;
+  height: 150px;
+  overflow: hidden;
+`;
+const CloseBtn = styled(Icon)` 
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    color: red;
+    display: block;
+`;
+const BannerImg = styled.img``;
 const Banner = () => {
-    return(<div>Banner</div>)
+    return (
+        <Base>
+            <CloseBtn>close</CloseBtn>
+            {/*TODO(hwanyb): 구글 설문조사 배너*/}
+            {/*- 이미지 src 채워야 함*/}
+            {/*assignees: hwanyb*/}
+            <BannerImg alt="구글 설문조사 배너"/>
+        </Base>
+    )
 }
 
 export default Banner;
