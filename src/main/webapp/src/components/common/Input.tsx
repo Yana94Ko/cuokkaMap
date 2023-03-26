4922523a24bdb824e92823a004a8cb7cc6d7f49c
hwanyb <mnn9502@naver.com>
2023-03-26 16:52:54 +0900

    [FE] Feat : Input 컴포넌트 구성

--- src/main/webapp/src/components/common/Input.tsx
+++ src/main/webapp/src/components/common/Input.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import styled from "styled-components";
 
-const Input = () => {
-    return(<div>Input</div>)
+const Base = styled.input``;
+
+
+type Props = {
+    value: string
+}
+const Input = ({value}: Props) => {
+    return (
+        <Base type="text" value={value}></Base>
+    )
 }
 
 export default Input;
