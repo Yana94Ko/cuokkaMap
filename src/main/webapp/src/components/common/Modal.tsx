0bf9f99999cf4c629e9b8c3ac5307d62b9d16ef4
hwanyb <mnn9502@naver.com>
2023-03-26 16:52:28 +0900

    [FE] Feat : Modal 컴포넌트 구성

--- src/main/webapp/src/components/common/Modal.tsx
+++ src/main/webapp/src/components/common/Modal.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, {ReactNode} from 'react';
+import styled from "styled-components";
 
-const Modal = () => {
-    return(<div>Modal</div>)
+// TODO(FE): Modal 컴포넌트 todo
+// close btn 추가해야함
+// 영역 밖 클릭시 e.target !== e.currentTarget 으로 모달 꺼지게 해야함
+// assignsees: hwanyb
+
+const Base = styled.div`
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  z-index: 999;
+  left: 0;
+  top: 0;
+  backdrop-filter: blur(5px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+type Props = {
+    children: ReactNode,
+}
+const Modal = ({children}: Props) => {
+    return (
+        <Base>{children}</Base>
+    )
 }
 
 export default Modal;
