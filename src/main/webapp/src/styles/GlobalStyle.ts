4a850ea2017b4c526c730eeec1793cf2d82550eb
hwanyb <mnn9502@naver.com>
2023-03-28 20:07:56 +0900

    [FE] Design : html fontsize 변경

--- src/main/webapp/src/styles/GlobalStyle.ts
+++ src/main/webapp/src/styles/GlobalStyle.ts
@@ -3,39 +3,11 @@ import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
-
-  @font-face {
-    font-family: 'Noto Sans KR Light', sans-serif;
-    src: url(${process.env.PUBLIC_URL} + "/assets/font/NotoSansKR-Light.otf");
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: 'Noto Sans KR Regular', sans-serif;
-    src: url(${process.env.PUBLIC_URL} + "/assets/font/NotoSansKR-Regular.otf");
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: 'Noto Sans KR Bold', sans-serif;
-    src: url(${process.env.PUBLIC_URL} + "/assets/font/NotoSansKR-Bold.otf");
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: 'Noto Sans KR Balck', sans-serif;
-    src: url(${process.env.PUBLIC_URL} + "/assets/font/NotoSansKR-Black.otf");
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: 'Gill Sans Ultra Bold', sans-serif;
-    src: url(${process.env.PUBLIC_URL} + "/assets/font/GILSANUB.TTF");
-    font-style: normal;
-  }
-
+  
   * {
     box-sizing: border-box;
+    margin: 0;
+    padding: 0;
   }
 
   body {
@@ -43,16 +15,13 @@ const GlobalStyle = createGlobalStyle`
   }
 
   html {
-    font-family: 'Noto Sans KR Reqular', sans-serif;
-    font-size: 16px;
-    @media ${props => props.theme.windowSize.desktop} {
-      font-size: 15px;
-    }
+    font-family: 'Noto Sans KR', sans-serif;
+    font-size: 15px;
     @media ${(props) => props.theme.windowSize.tablet} {
       font-size: 14px;
     }
     @media ${(props) => props.theme.windowSize.mobile} {
-      font-size: 13px;
+      font-size: 12px;
     }
   }
 `;
