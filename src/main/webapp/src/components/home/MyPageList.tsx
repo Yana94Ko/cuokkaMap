434894b637e3be7939fd5d785a760b1780df8f66
hwanyb <mnn9502@naver.com>
2023-03-30 10:51:15 +0900

    [FE] Design : 회원관련 드롭다운 메뉴 스타일 적용

--- src/main/webapp/src/components/home/MyPageList.tsx
+++ src/main/webapp/src/components/home/MyPageList.tsx
@@ -1,30 +1,47 @@
 import React from 'react';
 import styled from "styled-components";
-import axios from "axios";
-import {Link} from 'react-router-dom';
 
 const Base = styled.div`
   position: absolute;
-    background-color:white;
-  //padding-left:10vw;
+  right: 3rem;
+  background-color: white;
+  padding: 2rem;
+  border-radius: 1rem;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
 `;
 
-const MyPageList:React.FC = () => {
+const List = styled.ul`
+  border-radius: 1rem;
+`;
+const Item = styled.li`
+  transition: all 0.3s ease-in-out;
+  color: ${props => props.theme.color.darkGray};
+  cursor: pointer;
+  margin-bottom: 10px;
+  &:last-child{
+    margin-bottom: 0;
+  }
+  &:hover {
+    color: ${props => props.theme.color.primary};
+  }
+`;
+
+const MyPageList: React.FC = () => {
 
 
-    return(
+    return (
         <Base>
-            <ul>
-                <li>cuokkamap@gmail.com</li>
-                <li>내 사진</li>
-                <li>내 후기</li>
-                <li>의견 보내기</li>
-                <li onClick={() => {
+            <List>
+                <Item>cuokkamap@gmail.com</Item>
+                <Item>내 사진</Item>
+                <Item>내 후기</Item>
+                <Item>의견 보내기</Item>
+                <Item onClick={() => {
                     sessionStorage.removeItem("id");
                     window.location.reload();
                 }
-                }>로그아웃</li>
-            </ul>
+                }>로그아웃</Item>
+            </List>
         </Base>
     )
 }
