e2034c99a6cb43e844b3eeac7d2b865d64463843
hwanyb <mnn9502@naver.com>
2023-03-31 04:57:43 +0900

    [FE] Feat/Remove/Design - App.tsx 마이페이지 라우팅이 MyPageList로 되어있어 MyPage 를 Page 디렉토리로 이동시킨 후 라우팅 element MyPage로 수정 - MyPage 헤더 생성(로그아웃기능 구현, 로그아웃 후 인터셉트기능 구현)/ 스타일 적용 - MyPage 마크업, 기능구현 및 스타일적용 (로그인 상태 감지하여 미로그인 상태시 인터셉트 기능, name과 id로 currentMyPageView관리하여 조건부렌더링) - 사진, 후기, 북마크 컴포넌트 생성 - 추후 사진, 후기, 북마크 컴포넌트에서 공통적으로 사용할 Card 컴포넌트 생성 - 추후 마이페이지 관련 TODO 작성

---
+++ src/main/webapp/src/components/mypage/Header.tsx
@@ -0,0 +1,54 @@
+import React, {useEffect} from "react";
+import styled from "styled-components";
+import {Link, useNavigate} from "react-router-dom";
+import {useSelector} from "react-redux";
+
+import {Button, Icon} from "../../styles/common";
+import {RootState} from "../../modules";
+
+const Base = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
+const StyledLink = styled(Link)``;
+const Logo = styled.img`
+  height: 50px;
+`;
+const LogoutButton = styled(Button)`
+  width: 40px;
+  height: 40px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+
+const Header = () => {
+    const navigate = useNavigate();
+
+    const onLogoutClick = () => {
+        const result = window.confirm("로그아웃 하시겠습니까?");
+        if (result) {
+            sessionStorage.removeItem("id");
+            navigate("/");
+        }
+    }
+    return (
+        <Base>
+            <StyledLink to="/">
+                <Logo src={process.env.PUBLIC_URL + "/assets/images/logo/logo.png"} alt="로고이미지"/>
+            </StyledLink>
+            <LogoutButton onClick={onLogoutClick}>
+                <Icon className="material-symbols-rounded">logout</Icon>
+            </LogoutButton>
+            {/*TODO(FE): 회원탈퇴 기능 추가해야함*/}
+            {/*회원탈퇴관련 DB완료되면 기능 추가하기*/}
+            {/*assignees: hwanyb, SeongSilver*/}
+        </Base>
+    )
+}
+
+export default Header;
