b264c701b6261e0cc799e08bd4d1051068fff629
hwanyb <mnn9502@naver.com>
2023-04-05 16:04:26 +0900

    [FE] Design : 헤더 부분과 리뷰 탭부분 stick를 위한 구조 변경

--- src/main/webapp/src/components/home/CafeInfo.tsx
+++ src/main/webapp/src/components/home/CafeInfo.tsx
@@ -1,8 +1,7 @@
 import React, {useState, useRef, useEffect} from 'react';
 import styled, {css} from "styled-components"
 
-import {CloseBtn} from "./PostCafeInfo";
-import {Button, Icon, Tag} from "../../styles/common";
+import {Icon, Tag} from "../../styles/common";
 import {useDispatch, useSelector} from "react-redux";
 import {setIsOpenedCafeInfo} from "../../modules/viewReducer";
 import CafeInfoPhotoReview from "./review/CafeInfoPhotoReview";
@@ -15,21 +14,22 @@ const Base = styled.div`
   position: absolute;
   z-index: 1000;
   top: 15vh;
-  left: 50px;
-  padding: 2rem;
+  left: 3rem;
   box-shadow: 2px 0 5px rgba(0, 0, 0, 0.2);
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   border-radius: 1.5rem;
-  overflow-y:scroll;
-  &::-webkit-scrollbar{
-    display:none;
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    display: none;
   }
 
   @media ${props => props.theme.windowSize.laptop} {
+    height: 70vh;
     top: 50%;
-    transform: translateY(-50%);
+    transform: translateY(-48%);
   }
   @media ${props => props.theme.windowSize.tablet} {
     width: 350px;
@@ -46,32 +46,37 @@ const Base = styled.div`
   }
 `;
 
-const CafeInfoWrapper = styled.div`
-  a {
-    outline: none;
-    text-decoration: none;
-    color: black;
-    transition: 0.1s ease-in-out;
+const CloseBtn = styled(Icon)`
+  position: absolute;
+  top: 2rem;
+  right: 2rem;
+  transition: all 0.2s ease-in-out;
 
-    &:hover {
-      color: rgb(51, 134, 255);
-    }
-  }`;
+  &:hover {
+    transform: rotate(90deg);
+  }
+`;
+const CafeInfoWrapper = styled.div``;
 const TitleWrapper = styled.div`
-    display:flex;
+  display: flex;
+  position: sticky;
+  position: -webkit-sticky;
+  z-index: 11;
+  top: 0;
+  left: 0;
+  background-color: #fff;
+  padding: 2rem;
 `;
 
-
-const PlaceName = styled.h3`
+const PlaceName = styled.a`
   font-size: ${props => props.theme.fontSize.lg};
   font-weight: 900;
-  margin-bottom: 2rem;
+  color: #000;
+  transition: all 0.3s ease-in-out;
+  text-decoration: none;
 
-  a {
-    img {
-      position: absolute;
-      top: 27px;
-    }
+  &:hover {
+    color: ${props => props.theme.color.primary};
   }
 `;
 
@@ -79,6 +84,7 @@ const Item = styled.div`
   display: flex;
   line-height: 2rem;
   margin-bottom: 1.5rem;
+  padding: 0 2rem 0 2rem;
 `;
 
 const LabelIcon = styled(Icon)`
@@ -97,28 +103,30 @@ const Label = styled.label`
   font-size: ${props => props.theme.fontSize.base};
   font-weight: 700;
   margin-bottom: 20px;
+  padding-left: 2rem;
 `;
 
-const InfoRequestBtn = styled(Button)`
+const InfoRequestBtn = styled.a`
   width: fit-content;
   margin: 0 auto;
   margin-top: 1rem;
   font-size: ${props => props.theme.fontSize.sm};
   background-color: ${props => props.theme.color.gray};
-  padding: 0.2rem 1rem;
+  padding: 0.5rem 1rem;
   display: block;
   color: ${props => props.theme.color.text};
+  border-radius: 1rem;
 
-  a {
-    outline: none;
-    text-decoration: none;
-    color: white;
+  &:visited {
+    color: ${props => props.theme.color.text};
   }
 `;
 
 const TagWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
+  padding: 0 2rem 0 2rem;
+
   @media ${props => props.theme.windowSize.mobile} {
     flex-wrap: nowrap;
     width: 100vw;
@@ -140,20 +148,24 @@ const StyledTag = styled(Tag)`
 
 const ReviewTab = styled.div`
   display: flex;
+  position: sticky;
+  position: -webkit-sticky;
+  z-index: 11;
+  top: 70px;
+  background-color: #fff;
+  padding: 2rem 0 0 0;
   justify-content: space-between;
 `;
 
 const Tab = styled.div<{ currentView: string }>`
   width: 50%;
   text-align: center;
-  margin-top: 2rem;
   padding-bottom: 10px;
   border-bottom: 2px solid ${props => props.theme.color.gray};
   transition: all 0.2s ease-in-out;
   font-weight: 300;
   color: ${props => props.theme.color.darkGray};
   cursor: pointer;
-  margin-bottom: 2rem;
   ${props => props.currentView === props.id && css`
     border-bottom: 2px solid ${props => props.theme.color.primary};
     color: ${props => props.theme.color.text};
@@ -161,12 +173,16 @@ const Tab = styled.div<{ currentView: string }>`
   `}
 `;
 
+const CafeReviewContent = styled.div`
+  padding: 2rem;
+`;
+
 type CafeInfoProps = {
     cafeInfoContainer: object;
 }
 
 const CafeInfo = ({cafeInfoContainer}: CafeInfoProps) => {
-    const [currentView, setCurrentView] = useState<string>("review");
+    const [currentView, setCurrentView] = useState<string>("photo");
 
     let dataObject: any = {};
     dataObject = Object.assign({}, cafeInfoContainer);
@@ -187,62 +203,68 @@ const CafeInfo = ({cafeInfoContainer}: CafeInfoProps) => {
     }
     return (
         <Base>
-            <CloseBtn className="material-symbols-rounded" onClick={closeCafeInfo}>close</CloseBtn>
             {
                 cafeInfoContainer !== undefined && (
+                    <>
+                        {
+                            dataObject.data.insta ? (
+                                <TitleWrapper>
+                                    <CloseBtn className="material-symbols-rounded" onClick={closeCafeInfo}>close</CloseBtn>
+                                    <PlaceName href={dataObject.data.place_url} target="_blank">
+                                        {dataObject.data.place_name}
+                                    </PlaceName>
+                                    <a href={dataObject.data.insta} target="_blank">
+                                        <img className="insta"
+                                             src={process.env.PUBLIC_URL + "/assets/images/markers/insta.png"}
+                                             width="30px" alt="insta"/>
+                                    </a>
+                                </TitleWrapper>
 
-                    <CafeInfoWrapper>
-                        <TitleWrapper>
-                        {dataObject.data.place_url ? (
-                            <a href={dataObject.data.place_url} target="_blank"><PlaceName>
-                                {dataObject.data.place_name}&emsp;
+                            ) : (
+                                <TitleWrapper>
+                                    <CloseBtn className="material-symbols-rounded" onClick={closeCafeInfo}>close</CloseBtn>
+                                    <PlaceName href={dataObject.data.place_url} target="_blank">
+                                        {dataObject.data.place_name}
+                                    </PlaceName>
+                                </TitleWrapper>
+                            )
+                        }
 
-                            </PlaceName></a>
-                        ) : (
-                            <PlaceName>{dataObject.data.place_name}</PlaceName>
-                        )}
-                        {dataObject.data.insta ? (
-                            <a href={dataObject.data.insta} target="_blank"><img className="insta"
-                                                                                 src={process.env.PUBLIC_URL + "/assets/images/markers/insta.png"}
-                                                                                 width="30px" alt="insta"/></a>
-                        ) : (
-                            <PlaceName>{dataObject.data.insta}</PlaceName>
-                        )}
-                        </TitleWrapper>
-                        <Item>
-                            <LabelIcon className="material-symbols-rounded">location_on</LabelIcon>
-                            {/*클릭한 카페 주소*/}
-                            <Info>{dataObject.data.road_address_name}</Info>
-                        </Item>
-                        <Item>
-                            <LabelIcon className="material-symbols-rounded">phone_enabled</LabelIcon>
-                            {/*클릭한 카페 전화번호*/}
-                            {dataObject.data.phone ? (<Info>{dataObject.data.phone}</Info>) : (<p>연락처 미등록</p>)}
-                        </Item>
-                        <Label>옵션</Label>
-                        <TagWrapper>
-                            {
-                                filterArr.map((tag: string, idx: number) => (
-                                    <StyledTag key={idx} clickable={false} active={false} style={{borderColor: "#3386FF"}}>{
-                                        tag === "decaf" ? "디카페인" : tag === "lactos" ? "락토프리 우유" : tag === "soy" ? "두유" : tag === "oat" ? "오트밀크" : tag === "zero" ? "제로시럽" : ""
-                                    }</StyledTag>
-                                ))
-                            }
-                        </TagWrapper>
-                        <a href="https://forms.gle/H3M3YwCPgqgHVoRn7"
-                           target="_blank"><InfoRequestBtn>정보수정요청</InfoRequestBtn></a>
-                    </CafeInfoWrapper>
+                        <CafeInfoWrapper>
+                            <Item>
+                                <LabelIcon className="material-symbols-rounded">location_on</LabelIcon>
+                                <Info>{dataObject.data.road_address_name}</Info>
+                            </Item>
+                            <Item>
+                                <LabelIcon className="material-symbols-rounded">phone_enabled</LabelIcon>
+                                {dataObject.data.phone ? (<Info>{dataObject.data.phone}</Info>) : (<p>연락처 미등록</p>)}
+                            </Item>
+                            <Label>옵션</Label>
+                            <TagWrapper>
+                                {
+                                    filterArr.map((tag: string, idx: number) => (
+                                        <StyledTag key={idx} clickable={false} active={false}
+                                                   style={{borderColor: "#3386FF"}}>{
+                                            tag === "decaf" ? "디카페인" : tag === "lactos" ? "락토프리 우유" : tag === "soy" ? "두유" : tag === "oat" ? "오트밀크" : tag === "zero" ? "제로시럽" : ""
+                                        }</StyledTag>
+                                    ))
+                                }
+                            </TagWrapper>
+                            <InfoRequestBtn href="https://forms.gle/H3M3YwCPgqgHVoRn7"
+                                            target="_blank">정보수정요청</InfoRequestBtn>
 
+                        </CafeInfoWrapper>
+                    </>
+                )}
 
-                )
-            }
             <ReviewTab onClick={onReviewTabClick}>
                 <Tab id="photo" currentView={currentView}>사진</Tab>
                 <Tab id="review" currentView={currentView}>후기</Tab>
             </ReviewTab>
-            {
-                currentView === "photo" ? <CafeInfoPhotoReview cafeInfoContainer={cafeInfoContainer}/> : <CafeInfoReview/>
-            }
+            <CafeReviewContent>
+                {currentView === "photo" ? <CafeInfoPhotoReview cafeInfoContainer={cafeInfoContainer}/> :
+                    <CafeInfoReview cafeInfoContainer={cafeInfoContainer}/>}
+            </CafeReviewContent>
         </Base>
     )
 }
