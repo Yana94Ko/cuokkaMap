7837b44bce3dae4a96e9d504a6f85a446c0be4b0
SeongSilver <95121282+SeongSilver@users.noreply.github.com>
2023-04-06 16:24:38 +0900

    [FE] Feat : 상단 필터태그에 북마크 태그도 추가

--- src/main/webapp/src/modules/filterReducer.ts
+++ src/main/webapp/src/modules/filterReducer.ts
@@ -1,21 +1,28 @@
 const SET_CURRENT_FILTER = "filterReducer/SET_CURRENT_FILTER" as const;
+const SET_IS_BOOKMARK_MODE = "filterReducer/SET_IS_BOOKMARK_MODE" as const;
 
 
 export const setCurrentFilter = (currentFilter: object) => ({
-
     type: SET_CURRENT_FILTER,
     payload: currentFilter
 });
+export const setIsBookmarkMode = (isBookmarkMode: boolean) => ({
+    type: SET_IS_BOOKMARK_MODE,
+    payload: isBookmarkMode
+});
 
-type FilterAction = ReturnType<typeof setCurrentFilter>;
+type FilterAction = ReturnType<typeof setCurrentFilter> | ReturnType<typeof setIsBookmarkMode>;
 
 type FilterState = {
     currentFilter: any
+    isBookmarkMode : boolean
 }
 
 const initialState: FilterState = {
-    currentFilter: []
+    currentFilter: [],
+    isBookmarkMode: false,
 }
+
 
 function filterReducer(
     state: FilterState = initialState,
@@ -25,6 +32,10 @@ function filterReducer(
         case SET_CURRENT_FILTER:
             return {
                 ...state, currentFilter: action.payload
+            };
+        case SET_IS_BOOKMARK_MODE:
+            return {
+                ...state, isBookmarkMode: action.payload
             };
         default:
             return state;
