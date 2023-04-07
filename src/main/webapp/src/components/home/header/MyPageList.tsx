8b7669f936261105ea34bc767fe53881a2f31cfb
hwanyb <mnn9502@naver.com>
2023-04-07 16:39:41 +0900

    [FE] Feat / Design - 컴포넌트 구조 변경 - 컴포넌트 파일명 변경 - 마이페이지 리스트 수정, 링크연결 등 - 마이페이지 리스트 스타일 적용 - 마이페이지 로그아웃 아이콘에서 버튼으로 변경

---
+++ src/main/webapp/src/components/home/header/MyPageList.tsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import styled from "styled-components";
+import {useNavigate} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+
+import {RootState} from "../../../modules";
+import {setIsOpenedLoginModal} from "../../../modules/userReducer";
+import {setCurrentMyPageView} from "../../../modules/viewReducer";
+
+const Base = styled.div`
+  position: absolute;
+  top: 105%;
+  left: -90%;
+  background-color: white;
+  border-radius: 0.5rem;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+  overflow: hidden;
+`;
+
+const List = styled.ul``;
+
+const Item = styled.li`
+  transition: all 0.2s ease-in-out;
+  color: ${props => props.theme.color.text};
+  font-weight: 700;
+  cursor: pointer;
+  text-align: center;
+  white-space: nowrap;
+  padding: 1rem 1.5rem;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+
+  &:hover {
+    color: ${props => props.theme.color.primary};
+    background-color: ${props => props.theme.color.lightGray};
+  }
+`;
+
+const StyledA = styled.a`
+  transition: all 0.3s ease-in-out;
+  text-decoration: none;
+  color: ${props => props.theme.color.text};
+
+  &:visited {
+    color: ${props => props.theme.color.text};
+  }
+
+  &:hover {
+    color: ${props => props.theme.color.primary};
+  }
+`;
+
+const MyPageList: React.FC = () => {
+    const dispatch = useDispatch();
+
+    const isLoggedin = useSelector((state: RootState) => state.userReducer.isLoggedin);
+
+    const navigate = useNavigate();
+
+    const onLogoutClick = () => {
+        const result = window.confirm("로그아웃 하시겠습니까?");
+        if (result) {
+            sessionStorage.removeItem("id");
+            window.location.reload();
+        }
+    }
+
+    const onItemClick = (e: React.MouseEvent<HTMLUListElement>) => {
+        if (e.target instanceof Element) {
+            navigate('/mypage');
+            dispatch(setCurrentMyPageView(e.target.id))
+        }
+    }
+    return (
+        <Base>
+            {
+                isLoggedin ? (
+                    <List onClick={onItemClick}>
+                        <Item id="photo">내 사진</Item>
+                        <Item id="review">내 후기</Item>
+                        <Item id="bookmark">내 북마크</Item>
+                        <StyledA href="https://tough-dietician-fdf.notion.site/907b20e0956443a589d6ec3a041457cb"
+                                 target="_blank">
+                            <Item>
+                                고객센터
+                            </Item>
+                        </StyledA>
+                        <Item onClick={onLogoutClick}>로그아웃</Item>
+                    </List>
+                ) : (
+                    <List>
+                        <Item onClick={() => dispatch(setIsOpenedLoginModal(true))}>로그인</Item>
+                        <StyledA href="https://tough-dietician-fdf.notion.site/907b20e0956443a589d6ec3a041457cb"
+                                 target="_blank">
+                            <Item>
+                                고객센터
+                            </Item>
+                        </StyledA>
+                    </List>
+                )
+            }
+        </Base>
+    )
+}
+
+export default MyPageList
