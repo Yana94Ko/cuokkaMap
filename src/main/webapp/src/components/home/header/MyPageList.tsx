151ad6974af8c3e15560ef68030e3463fa02b36f
hwanyb <mnn9502@naver.com>
2023-04-12 18:46:04 +0900

    [FE] Feat : 마이페이지 내 로그아웃 버튼 없애고 홈화면과 동일한 리스트형식으로 변경 / 탈퇴 추가

--- src/main/webapp/src/components/home/header/MyPageList.tsx
+++ src/main/webapp/src/components/home/header/MyPageList.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
-import styled from "styled-components";
-import {useNavigate} from "react-router-dom";
+import styled, {css} from "styled-components";
+import {useLocation, useNavigate} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 
 import {RootState} from "../../../modules";
@@ -12,7 +12,7 @@ import {
     setIsOpenedPostCafe
 } from "../../../modules/viewReducer";
 
-const Base = styled.div`
+const Base = styled.div<{ location: string }>`
   width: 100px;
   position: absolute;
   top: 110%;
@@ -25,6 +25,12 @@ const Base = styled.div`
     width: 70px;
     left: 0;
   }
+  ${props => props.location === "/mypage" && css`
+    @media ${props => props.theme.windowSize.mobile} {
+      width: 70px;
+      left: 25%;
+    }
+  `}
 `;
 
 const List = styled.ul``;
@@ -42,12 +48,17 @@ const Item = styled.li`
     margin-bottom: 0;
   }
 
-  @media (hover: hover) {
-    &:hover {
-      color: ${props => props.theme.color.primary};
-      background-color: ${props => props.theme.color.lightGray};
-    }
+  ${props => props.id === "secession" && css`
+    border-top: 1px solid ${props.theme.color.gray};
+    color: ${props => props.theme.color.darkGray};
+    font-weight: 300;
+    font-size: ${props.theme.fontSize.sm};
+  `} @media(hover: hover) {
+  &:hover {
+    color: ${props => props.theme.color.primary};
+    background-color: ${props => props.theme.color.lightGray};
   }
+}
 
   @media ${props => props.theme.windowSize.mobile} {
     padding: 1rem 0;
@@ -78,6 +89,8 @@ const MyPageList: React.FC = () => {
 
     const navigate = useNavigate();
 
+    const location = useLocation();
+
     const onLogoutClick = () => {
         const result = window.confirm("로그아웃 하시겠습니까?");
         if (result) {
@@ -85,6 +98,8 @@ const MyPageList: React.FC = () => {
             window.location.reload();
         }
     }
+
+    // console.log(location.pathname)
 
     const onItemClick = (e: React.MouseEvent<HTMLUListElement>) => {
         if (e.target instanceof Element) {
@@ -97,20 +112,43 @@ const MyPageList: React.FC = () => {
             } else return;
         }
     }
+    const onSecessionClick = () => {
+        const result = window.confirm("지금 탈퇴하시면 추가하신 즐겨찾기, 후기, 사진 등 정보가 모두 삭제되며, 복구 불가능합니다."
+            + "\n" + "정말 탈퇴하시겠습니까?");
+        if (result) {
+            // TODO(FE): 탈퇴 api 연동하기
+            // assignees: hwanyb
+            // window.alert("탈퇴되셨습니다.");
+            // sessionStorage.removeItem("id");
+            // navigate("/");
+            // window.location.reload();
+        } else return;
+    }
     return (
-        <Base>
+        <Base location={location.pathname}>
             {
                 isLoggedin ? (
-                    <List onClick={onItemClick}>
-                        <Item className="myPageList" id="favorite">내 즐겨찾기</Item>
-                        <Item className="myPageList" id="photo">내 사진</Item>
-                        <Item className="myPageList" id="review">내 후기</Item>
-                        <StyledA href="https://tough-dietician-fdf.notion.site/907b20e0956443a589d6ec3a041457cb"
-                                 target="_blank">
-                            <Item className="myPageList">고객센터</Item>
-                        </StyledA>
-                        <Item className="myPageList" onClick={onLogoutClick}>로그아웃</Item>
-                    </List>
+                    location.pathname === "/mypage" ? (
+                        <List onClick={onItemClick}>
+                            <Item className="myPageList" onClick={onLogoutClick}>로그아웃</Item>
+                            <StyledA href="https://tough-dietician-fdf.notion.site/907b20e0956443a589d6ec3a041457cb"
+                                     target="_blank">
+                                <Item className="myPageList">고객센터</Item>
+                            </StyledA>
+                            <Item id="secession" className="myPageList" onClick={onSecessionClick}>회원탈퇴</Item>
+                        </List>
+                    ) : (
+                        <List onClick={onItemClick}>
+                            <Item className="myPageList" id="favorite">내 즐겨찾기</Item>
+                            <Item className="myPageList" id="photo">내 사진</Item>
+                            <Item className="myPageList" id="review">내 후기</Item>
+                            <StyledA href="https://tough-dietician-fdf.notion.site/907b20e0956443a589d6ec3a041457cb"
+                                     target="_blank">
+                                <Item className="myPageList">고객센터</Item>
+                            </StyledA>
+                            <Item className="myPageList" onClick={onLogoutClick}>로그아웃</Item>
+                        </List>
+                    )
                 ) : (
                     <List>
                         <Item className="myPageList" onClick={() => dispatch(setIsOpenedLoginModal(true))}>로그인</Item>
