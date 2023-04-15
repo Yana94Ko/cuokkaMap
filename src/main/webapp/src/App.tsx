399b50fb5dc0ee8019c8fa6ac7a1022a6d04776b
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2023-04-15 10:57:32 +0900

    main 어드민 라우팅 아예 삭제

--- src/main/webapp/src/App.tsx
+++ src/main/webapp/src/App.tsx
@@ -46,7 +46,6 @@ function App() {
             <Routes>
                 <Route path="/" element={<HomePage />}/>
                 <Route path="/mypage" element={<MyPage />}/>
-<!--                 <Route path="/admin" element={<AdminPage />}/> -->
                 {/*↓로그인 콜백 라우팅용*/}
                 <Route path="/login/callback" element={<KakaoOauth2RedirectHandler/>}></Route>
             </Routes>
