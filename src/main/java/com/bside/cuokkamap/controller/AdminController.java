b41291b00041ea4c6529341d98ee68a52b0abe38
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2023-08-29 16:37:23 +0900

    [todos] todo-to-issue test

--- src/main/java/com/bside/cuokkamap/controller/AdminController.java
+++ src/main/java/com/bside/cuokkamap/controller/AdminController.java
@@ -1,22 +1,58 @@
 package com.bside.cuokkamap.controller;
 
 public class AdminController {
-    //Todo(BE, admin) : 카페 검색(이름)
-    //Todo(BE, admin) : 전체 카페 수 카운트
-    //Todo(BE, admin) : 등록된 카페의 목록을 확인 할 수 있는 기능
-    //Todo(BE, admin) : 카페 삭제 기능
-    //Todo(BE, admin) : 카페 기본 정보를 확인 할 수 있는 기능
-    //Todo(BE, admin) : 등록된 사진과 업로더를 확인 할 수 있는 기능
-    //Todo(BE, admin) : 등록된 후기와 업로더를 확인 할 수 있는 기능
-    //Todo(BE, admin) : 카페 정보 수정(연락처, 커스텀 한정)
-    //Todo(BE, admin) : 카페 정보에 등록된 사진을 (다중)삭제 할 수 있는 기능
-    //Todo(BE, admin) : 카페 정보에 등록된 후기를 (다중)삭제 할 수 있는 기능
-    //Todo(BE, admin) : 회원 id 또는 이메일로 회원 검색하는 기능
-    //Todo(BE, admin) : 전체 회원 수 카운트(차단 회원 제외)
-    //Todo(BE, admin) : 회원 목록을 확인 할 수 있는 기능
-    //Todo(BE, admin) : 회원 정보를 확인 할 수 있는 기능(이메일 한정)
-    //Todo(BE, admin) : (다중)회원을 삭제(=탈퇴) 할 수 있는 기능
-    //Todo(BE, admin) : (다중)회원 차단 기능(차단시 회원이 등록한 모든 정보 삭제, 회원 id 와 이메일 정보 보유)
-    //Todo(BE, admin) : 차단된 회원 목록을 확인 할 수 있는 기능
-    //Todo(BE, admin) : 차단된 회원을 삭제 할 수 있는 기능
+    // Todo(BE, admin) : 카페 검색(이름)
+    // assignees: Yana94Ko
+
+    // Todo(BE, admin) : 전체 카페 수 카운트
+    // assignees: Yana94Ko
+
+    // Todo(BE, admin) : 등록된 카페의 목록을 확인 할 수 있는 기능
+    // assignees: Yana94Ko
+
+    // Todo(BE, admin) : 카페 삭제 기능
+    // assignees: Yana94Ko
+
+    // Todo(BE, admin) : 카페 기본 정보를 확인 할 수 있는 기능
+    // assignees: Yana94Ko
+
+    // Todo(BE, admin) : 등록된 사진과 업로더를 확인 할 수 있는 기능
+    // assignees: Yana94Ko
+
+    // Todo(BE, admin) : 등록된 후기와 업로더를 확인 할 수 있는 기능
+    // assignees: Yana94Ko
+
+    // Todo(BE, admin) : 카페 정보 수정(연락처, 커스텀 한정)
+    // assignees: Yana94Ko
+
+    // Todo(BE, admin) : 카페 정보에 등록된 사진을 (다중)삭제 할 수 있는 기능
+    // assignees: Yana94Ko
+
+    // Todo(BE, admin) : 카페 정보에 등록된 후기를 (다중)삭제 할 수 있는 기능
+    // assignees: Yana94Ko
+
+    // Todo(BE, admin) : 회원 id 또는 이메일로 회원 검색하는 기능
+    // assignees: Yana94Ko
+
+    // Todo(BE, admin) : 전체 회원 수 카운트(차단 회원 제외)
+    // assignees: Yana94Ko
+
+    // Todo(BE, admin) : 회원 목록을 확인 할 수 있는 기능
+    // assignees: Yana94Ko
+
+    // Todo(BE, admin) : 회원 정보를 확인 할 수 있는 기능(이메일 한정)
+    // assignees: Yana94Ko
+
+    // Todo(BE, admin) : (다중)회원을 삭제(=탈퇴) 할 수 있는 기능
+    // assignees: Yana94Ko
+
+    // Todo(BE, admin) : (다중)회원 차단 기능(차단시 회원이 등록한 모든 정보 삭제, 회원 id 와 이메일 정보 보유)
+    // assignees: Yana94Ko
+
+    // Todo(BE, admin) : 차단된 회원 목록을 확인 할 수 있는 기능
+    // assignees: Yana94Ko
+
+    // Todo(BE, admin) : 차단된 회원을 삭제 할 수 있는 기능
+    // assignees: Yana94Ko
+
 }
